a1f3f6dd3b7fd30c5593f4b5ea44683241af9d6b
Dmitry Zaitsev <dmitry.zaitsev.1993@gmail.com>
2020-05-02 23:41:53 +0200

    Adding project to repository.

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Calculator msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Calculator from './components/Calculator.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Calculator
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Calculator.vue
@@ -0,0 +1,147 @@
+<template>
+  <div>
+    <form name="form">
+      <header class="display element">
+        <div class="text a">{{ action || '123' }}</div>
+        <div class="text b">{{ current || '0' }}</div>
+      </header>
+    </form>
+    <table class="element">
+      <tr>
+        <td><input class="button" type="button" value="C" @click="clear"></td>
+        <td><input class="button" type="button" value="+/-" @click="sign"></td>
+        <td><input class="button" type="button" value="%" @click="percent"></td>
+        <td><input class="button operator" type="button" value="÷" @click="divide"></td>
+      </tr>
+      <tr>
+        <td><input class="button" type="button" value="7" @click="append('7')"></td>
+        <td><input class="button" type="button" value="8" @click="append('8')"></td>
+        <td><input class="button" type="button" value="9" @click="append('9')"></td>
+        <td><input class="button operator" type="button" value="x" @click="times"></td>
+      </tr>
+      <tr>
+        <td><input class="button" type="button" value="4" @click="append('4')"></td>
+        <td><input class="button" type="button" value="5" @click="append('5')"></td>
+        <td><input class="button" type="button" value="6" @click="append('6')"></td>
+        <td><input class="button operator" type="button" value="-" @click="minus"></td>
+      </tr>
+      <tr>
+        <td><input class="button" type="button" value="1" @click="append('1')"></td>
+        <td><input class="button" type="button" value="2" @click="append('2')"></td>
+        <td><input class="button" type="button" value="3" @click="append('3')"></td>
+        <td><input class="button operator" type="button" value="+" @click="plus"></td>
+      </tr>
+      <tr>
+        <td colspan=2><input class="button" style="width: 105px" type="button" value="0" @click="append('0')"></td>
+        <td><input class="button" type="button" value="." @click="dot"></td>
+        <td><input class="button operator" type="button" value="=" @click="equal"></td>
+      </tr>
+    </table>  
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      previous: null,
+      current: '',
+      operator: null,
+      operatorClicked: false
+    }
+  },
+  methods: {
+    clear(){
+      this.action = '',
+      this.current = ''
+    },
+    sign() {
+      this.current = this.current.charAt(0) === '-' ?
+      this.current.slice(1) : `-${this.current}`
+    },
+    percent() {
+      this.current = `${parseFloat(this.current) / 100}`
+    },
+    append(number){
+      if(this.operatorClicked){
+        this.current = ''
+        this.operatorClicked = false
+      }
+
+      this.current = `${this.current}${number}`
+    },
+    dot(){
+      if(this.current.indexOf('.') === -1){
+        this.append('.');
+      }
+    },
+    setPrevious(){
+      this.previous = this.current
+      this.operatorClicked = true
+    },
+    divide(){
+      this.operator = (a, b) => a / b
+      this.setPrevious()
+    },
+    times(){
+      this.operator = (a, b) => a * b
+      this.setPrevious()
+    },
+    minus(){
+      this.operator = (a, b) => a - b
+      this.setPrevious()
+    },
+    plus(){
+      this.operator = (a, b) => a + b
+      this.setPrevious()
+    },
+    equal(){
+      this.current = `${ this.operator(parseFloat(this.current), parseFloat(this.previous)) }`
+      this.previous = null
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.display{
+  width: 213px;
+  height: 100px;
+  padding: 5;
+  background-color: black;
+}
+.text{
+  height: 30px;
+  font-size: 30px;
+  text-align: right;
+  color: white;
+  padding: 8px;
+}
+
+.a{
+  font-size: 20px;
+  color: gray;
+}
+
+.element{
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.button{
+  width: 50px;
+  height: 50px;
+  font-family: 'Courier New', Courier, monospace;
+  font-size: 20px;
+  background-color:#252525;
+  color: white;
+  cursor: pointer;
+  border: none;
+}
+
+.operator{
+  background-color: #ffc309;
+  color: #7b5904;
+}
+</style>
